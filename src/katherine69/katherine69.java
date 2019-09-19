2df0b1af268b2dc03b1b8dc46358fed0ef340e2f
USUARIO <USUARIO@PC4>
2019-09-19 10:26:17 -0500

    saludos desde otro pais

--- src/katherine69/katherine69.java
+++ src/katherine69/katherine69.java
@@ -1,10 +1,42 @@
 
 package katherine69;
 
+import java.util.Scanner;
+
 public class katherine69 {
 
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
-        System.out.println("maicoljack me fascina");
+        Scanner obj = new Scanner(System.in);
+        int opcion;
+        System.out.println("elija una opcion de saludo en diferentes idiomas");
+        System.out.println("1. español");
+        System.out.println("2. ingles");
+        System.out.println("3. griego");
+        System.out.println("4. noruego");
+        System.out.println("5. aleman");
+        opcion = obj.nextInt();
+        
+        switch(opcion){
+            case 1:
+                System.out.println("HOLA MUUUUUUUUUNDO");
+            break;
+            case 2:
+                System.out.println("HELLO WOOOOOOOOORLD");
+            break;
+            case 3:
+                System.out.println("γεια κόόόόόόόόόσμο");
+            break;
+            case 4:
+                System.out.println("hei veeeeeeeerden");
+            break;
+            case 5:
+                System.out.println("Hallo Weeeeeeeeelt");
+            break;
+            default:
+                System.out.println("elija una opcion valida");
+        }  
+        
     }
     
 }
